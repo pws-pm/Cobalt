46ef96aee0cd09bdaa01d743d57d7f489ebe33c0
pws-pm <147876953+pws-pm@users.noreply.github.com>
2024-03-19 15:37:08 +0100

    test

--- tokens.config.mjs
+++ tokens.config.mjs
@@ -3,6 +3,6 @@ import pluginSass from "@cobalt-ui/plugin-sass";
 /** @type {import("@cobalt-ui/core").Config} */
 export default {
   tokens: "./tokens.json",
-  outDir: "./tokens/",
+  outDir: "./build/sass/",
   plugins: [pluginSass()], 
 };
