6467f5fb1ebadec11792e9325a4f33c6cff8e161
pws-pm <147876953+pws-pm@users.noreply.github.com>
2024-04-29 14:43:31 +0200

    manual commented xaml shadows

--- tokens.config.mjs
+++ tokens.config.mjs
@@ -1,8 +1,7 @@
 //import pluginCSS from "@cobalt-ui/plugin-css";
+// Instead of the cobalt plugin CSS use Figma Anima plugin CSS, add the inset shadows manually as a bug prevents to generate them
 import pluginSass from "@cobalt-ui/plugin-sass";
 import pluginMAUI from "./plugins/pluginMAUI.js";
-
-
 
 /** @type {import("@cobalt-ui/core").Config} */
 export default {
@@ -10,6 +9,7 @@ export default {
   outDir: "./_output",
   plugins: [
     pluginSass(),
+    //pluginCSS(),
     pluginMAUI({
       excludePatterns: ["^colorbase"]
     })
